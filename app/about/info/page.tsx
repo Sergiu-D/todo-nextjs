6188364bd7da1e28e0b04e42edf2528977bb89ed
Sergiu-D <61351750+Sergiu-D@users.noreply.github.com>
2023-09-30 15:04:53 +0300

    test commit

---
+++ app/about/info/page.tsx
@@ -0,0 +1,9 @@
+export default async function Info() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between font-mono p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+        <h1>About us info page</h1>
+      </div>
+    </main>
+  );
+}
